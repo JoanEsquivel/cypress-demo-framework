a8c9f77fba38492e31925f5d118bf41afb7bee79
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-06-05 14:38:01 -0700

    cypress 10 migration

---
+++ cypress.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  projectId: 'ioceso',
+  screenshotOnRunFailure: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/report',
+    charts: true,
+    reportPageTitle: 'Joan Media Demo Report',
+    embeddedScreenshots: true,
+  },
+  env: {
+    db: {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'cypress_test',
+    },
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
